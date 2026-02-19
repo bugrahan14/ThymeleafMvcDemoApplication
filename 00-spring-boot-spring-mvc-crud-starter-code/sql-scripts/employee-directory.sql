93adcea1913e5c7d9be7a3967c1c9686e5400ed8
bugrahan14 <ertasbugra0611@gmail.com>
2026-02-19 17:42:05 +0300

    sql script update

--- 00-spring-boot-spring-mvc-crud-starter-code/sql-scripts/employee-directory.sql
+++ 00-spring-boot-spring-mvc-crud-starter-code/sql-scripts/employee-directory.sql
@@ -20,9 +20,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
+	(1,'Leslie','Andrews','leslie@BugraERT.com'),
+	(2,'Emma','Baumgarten','emma@BugraERT.com'),
+	(3,'Avani','Gupta','avani@BugraERT.com'),
+	(4,'Yuri','Petrov','yuri@BugraERT.com'),
+	(5,'Juan','Vega','juan@BugraERT.com');
 
