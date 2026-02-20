e7976af660b7e8efd765e5273cdf03fd8b2f3bb9
bugrahan14 <ertasbugra0611@gmail.com>
2026-02-20 13:52:09 +0300

    Add Employee method thymeleaf

--- 00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/bugrahanERT/springboot/cruddemo/controller/EmployeeController.java
+++ 00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/bugrahanERT/springboot/cruddemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import com.bugrahanERT.springboot.cruddemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,6 +33,52 @@ public class EmployeeController {
         // add to the spring model
         theModel.addAttribute("employees", theEmployees);
 
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel){
+
+        // CREATE MODEL ATTRİBUTE TO BİND FORM DATA
+        Employee theEmployee= new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute ("employee") Employee theEmployee){
+
+        // SAVE THE EMPLOYEE
+        employeeService.save(theEmployee);
+
+        // UAE A REDİRECT TO PREVENT DUPLİCATE SUBMMSSİONS
+        return "redirect:/employees/list";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/bugrahanERT/springboot/cruddemo/dao/EmployeeRepository.java
+++ 00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/bugrahanERT/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,8 +3,13 @@ package com.bugrahanERT.springboot.cruddemo.dao;
 import com.bugrahanERT.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
+
+    // ADD A METHOD TO SORT BY LAST NAME
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
